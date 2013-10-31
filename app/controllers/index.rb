fd83c5e4077babc1cfc66a0f48dc3b99dc4b609b
lolcrzykid <mohitr35@gmail.com>
2013-10-31 12:41:31 -0700

    wip.r2 edit layout.erb to begin signout process

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -26,7 +26,6 @@ post '/sessions' do
   end
 end
 
-
 delete '/sessions/:id' do
 
   # sign-out  - use a partial with a form (no AJAX)
@@ -35,7 +34,6 @@ end
 #----------- USERS -----------
 
 get '/users/new' do
-
   # render sign-up page
   erb :sign_up
 end
@@ -45,5 +43,3 @@ post '/users' do
   # sign-up a new user
   "welcome new user: #{current_user.name}!"
 end
-
-#"session_id"=>"ddf63b23656765c4b093f5550200723bedd62e0157dec895ecc3d2a46199a08a", "tracking"=>{"HTTP_USER_AGENT"=>"98165fd680503272a3708d21632722a1dbff40ca", "HTTP_ACCEPT_ENCODING"=>"ed2b3ca90a4e723402367a1d17c8b28392842398", "HTTP_ACCEPT_LANGUAGE"=>"66eae971492938c2dcc2fb1ddc8d7ec3196037da"}, "csrf"=>"a30b950d40834cb3e5469ecda494b9c9b61413cbb79880fa0698cd088b03767c", "user_id"=>1}
