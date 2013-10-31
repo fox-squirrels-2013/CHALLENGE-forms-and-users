8f9cff4e884699c3095c95252f72e6a97f885424
lolcrzykid <mohitr35@gmail.com>
2013-10-31 11:19:21 -0700

    r1.wip create user, save to session

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -35,6 +35,7 @@ post '/users' do
   user = User.create params[:user]
   session[:user_id] = user.id
   # sign-up a new user
-  "welcome new user: #{user.name}!"
+  "welcome new user: #{current_user.name}!"
 end
 
+#"session_id"=>"ddf63b23656765c4b093f5550200723bedd62e0157dec895ecc3d2a46199a08a", "tracking"=>{"HTTP_USER_AGENT"=>"98165fd680503272a3708d21632722a1dbff40ca", "HTTP_ACCEPT_ENCODING"=>"ed2b3ca90a4e723402367a1d17c8b28392842398", "HTTP_ACCEPT_LANGUAGE"=>"66eae971492938c2dcc2fb1ddc8d7ec3196037da"}, "csrf"=>"a30b950d40834cb3e5469ecda494b9c9b61413cbb79880fa0698cd088b03767c", "user_id"=>1}
--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,7 +1,12 @@
 helpers do
 
+  # goal is to use current_user like this:
+  # current_user.name
+  # current_user.email
   def current_user
     # TODO: return the current user if there is a user signed in.
+    User.find(session[:user_id])
   end
 
 end
+
