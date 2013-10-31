c528f6591a33f5ff3f696e05aee6e5b38b7fe80d
TGOlson <tydotg@gmail.com>
2013-10-31 14:24:16 -0700

    updated database.rb for local dev

--- config/database.rb
+++ config/database.rb
@@ -27,6 +27,13 @@ db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatr
 
 DB_NAME = db.path[1..-1]
 
+
+# hard-code user and pw for local developemnt
+
+if ENV['RACK_ENV'] == "development"
+  db.user, db.password = 'postgres', 'postgres'
+end
+
 # Note:
 #   Sinatra::Application.environment is set to the value of ENV['RACK_ENV']
 #   if ENV['RACK_ENV'] is set.  If ENV['RACK_ENV'] is not set, it defaults
@@ -36,8 +43,8 @@ ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
   :host     => db.host,
   :port     => db.port,
-  :username => 'postgres',
-  :password => 'postgres',
+  :username => db.user,
+  :password => db.password,
   :database => DB_NAME,
   :encoding => 'utf8'
 )
