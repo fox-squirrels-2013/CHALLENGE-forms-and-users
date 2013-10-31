1624e1bed5416fa3b51eb9198911e3a7a58853f5
Apprentice <apprentice@dbc09.local>
2013-10-31 12:28:46 -0700

    adsfgadsfg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
   #TODO: Add validations for name, email
   #TODO : Use bcrypt to store hashed passwords and authenticate users
 end
